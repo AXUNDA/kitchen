5a5e7fa80b47f0ac4a1c2b6d3fddc7455034824f
AXUNDA <charlesemmanuelazunda@gmail.com>
2024-06-26 09:24:25 +0100

    added index route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import express, { Router } from "express";
+import express, { Router, Request, Response, NextFunction } from "express";
 import authRoutes from "./auth.routes";
 import userRoutes from "./user.routes";
 import vendorRoutes from "./vendor.routes";
@@ -7,5 +7,10 @@ const router: Router = express.Router();
 router.use("/auth", authRoutes);
 router.use("/store", vendorRoutes);
 router.use("/vendors", userRoutes);
+router.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    status: "active",
+  });
+});
 
 export { router };
